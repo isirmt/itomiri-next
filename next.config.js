6a18fd2e364b941eb0b7956636e8f88d6e301c4c
isirmt <git config --global user.email>
2022-04-21 00:14:39 +0900

    posts

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/
+})
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'mdx']
+})
+
 module.exports = nextConfig
